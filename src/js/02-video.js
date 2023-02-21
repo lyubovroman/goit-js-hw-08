a9339083f3b56d0a2c631ceebcaa801cf23a1c9f
lyubovroman <lyubovromanou@gmail.com>
2023-02-21 20:37:21 +0200

    new comm

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -29,32 +29,3 @@ player.setCurrentTime(savedTime());
 
 
 
-
-
-// player.on('play', onPlay);
-
-// player.on('eventName', function(data) {
-//     // data is an object containing properties specific to that event
-// });
-// {
-//     duration: 61.857
-//     percent: 0.049
-//     seconds: 3.034
-// }
-
-
-// {/* <iframe src="https://player.vimeo.com/video/76979871?h=8272103f6e" width="640" height="360" frameborder="0" allowfullscreen allow="autoplay; encrypted-media"></iframe>
-
-// <script src="https://player.vimeo.com/api/player.js"></script>
-// <script>
-//     const iframe = document.querySelector('iframe');
-//     const player = new Vimeo.Player(iframe);
-
-//     player.on('play', function() {
-//         console.log('played the video!');
-//     });
-
-//     player.getVideoTitle().then(function(title) {
-//         console.log('title:', title);
-//     });
-// </script> */}
